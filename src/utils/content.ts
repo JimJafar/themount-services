3906033307772450d7e51a64592cfb337b924d64
Jim Sangwine <jim@sangwine.net>
2024-09-04 22:56:13 +0100

    initial PWA setup & first CMS fetch

---
+++ src/utils/content.ts
@@ -0,0 +1,16 @@
+export const cmsApi = async (path: string) => {
+  const res = await fetch(`https://themount-cms.codeministry.net/api/${path}`, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_CMS_API_TOKEN}`,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch CMS API');
+  }
+
+  return await res.json();
+};
+
+export const getServices = () => cmsApi('services');
+
